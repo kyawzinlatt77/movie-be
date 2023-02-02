6473a04e96f8b88dfedd5a3c1d876ae8ea259416
kyawzinlatt <kyawzinlatt415@gmail.com>
2023-02-02 18:28:16 +0630

    a

--- config/plugins.js
+++ config/plugins.js
@@ -6,7 +6,7 @@ module.exports =  ({ env }) => ({
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: true,
+      playgroundAlways: false,
       depthLimit: 10,
       amountLimit: 100,
       apolloServer: {
